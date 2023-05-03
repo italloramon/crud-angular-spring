074e68c2195120164978d4f28ddc6bb5553825d3
italloramon <irvp@ic.ufal.br>
2023-05-03 17:13:09 -0300

    Error handling and MatDialog

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -12,8 +14,23 @@ export class CoursesComponent {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = coursesService.findAll();
+  constructor(
+    private coursesService: CoursesService,
+    private dialog: MatDialog
+    ) {
+    this.courses$ = coursesService.findAll()
+    .pipe(
+      catchError(error => {
+        this.onError('Error on loading the courses.');
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
 }
