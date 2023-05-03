520ef8f1ed77dec7e3dd0758a5d19098408a54af
italloramon <irvp@ic.ufal.br>
2023-05-03 10:01:53 -0300

    Add a service in Angular

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +8,11 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
-  courses: Course[] = [
-    {_id: '1', name: 'Angular', category: 'front-end'}
-  ];
+  courses: Course[];
   displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = coursesService.findAll();
+  }
 
 }
